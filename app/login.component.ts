659a7af85ff4aa76321549c4ee371dc91b9bc4ff
Sunil Kumar <sunil@unyscape.com>
2016-12-09 21:37:32 +0530

    login api implementation

--- app/login.component.ts
+++ app/login.component.ts
@@ -1,17 +1,32 @@
 import {Component, ViewChild, ElementRef, Renderer} from 'angular2/core'
+import {LoginInterface} from './login.interface'
+import {LoginService} from './login.service'
 
 @Component({
     selector: 'login',
-    templateUrl: 'login-form.html'
+    templateUrl: 'login-form.html',
+    providers: [LoginService]
 })
 
 export class LoginComponent {
     @ViewChild('loginEmail') loginEmail:ElementRef
     @ViewChild('loginPass') loginPass:ElementRef
-    private userEmail
-    private userPass
+    
+    private posts;
+
+    constructor(private loginS: LoginService) {}
 
     authUser() {
         
+        let userData = {
+            "email": this.loginEmail.nativeElement.value,
+            "password": this.loginPass.nativeElement.value
+        }
+        this.loginS.authUser(userData).subscribe()
+
+        this.loginS.getPosts().subscribe(posts => {
+            this.posts = posts 
+        })
+        // console.log(this.posts)
     }
 }
---
+++ app/login.interface.ts
@@ -0,0 +1,4 @@
+export interface LoginInterface {
+    code: number
+    data: any[]
+}
